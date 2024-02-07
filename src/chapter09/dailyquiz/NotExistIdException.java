8ff0cf32ead5dc2661e1c6d986a1d1cc43f75356
cmleem <coals722@naver.com>
2024-02-07 11:10:07 +0900

    Exception daily quiz 240206

---
+++ src/chapter09/dailyquiz/NotExistIdException.java
@@ -0,0 +1,8 @@
+package chapter09.dailyquiz;
+
+public class NotExistIdException extends Exception {
+
+    public NotExistIdException(String message) {
+        super(message);
+    }
+}
---
+++ src/chapter09/dailyquiz/WrongPasswordException.java
@@ -0,0 +1,8 @@
+package chapter09.dailyquiz;
+
+public class WrongPasswordException extends Exception {
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
